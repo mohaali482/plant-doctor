a84de123cd5e062678eea4f54fd2b604ebe0b2b6
Mohammed Ali <mohaali482@gmail.com>
2023-09-02 11:31:24 +0300

    Add registration html form

--- plant_doctor/authentication/urls.py
+++ plant_doctor/authentication/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 
+from .views import RegisterView
+
 urlpatterns = [
     path('login/', LoginView.as_view(template_name="authentication/login.html"), name="login"),
     path('logout/', LogoutView.as_view(template_name="authentication/logout.html"), name='logout'),
+    path('register/', RegisterView.as_view(), name='register'),
 ]
--- plant_doctor/authentication/views.py
+++ plant_doctor/authentication/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+from .forms import SignupForm
+
+UserModel = get_user_model()
+
+class RegisterView(CreateView):
+    model = UserModel
+    form_class = SignupForm
+    template_name = "authentication/register.html"
+    success_url = reverse_lazy('auth:login')
